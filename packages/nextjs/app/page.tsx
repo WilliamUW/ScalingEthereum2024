e037b0131aa35627218a4a43443f677868ca1909
William Wang <bwilliamwang@gmail.com>
2024-04-10 22:25:08 -0400

    working nft fetch

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,10 +1,16 @@
 "use client";
 
-import Link from "next/link";
+import { Button } from "antd";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
+
+const baseURL = "https://eth-mainnet.g.alchemy.com/v2/374l9-eucheJf7r_lvnEZbEJ3dmtKRqn";
+
+const requestOptions: RequestInit = {
+  method: "GET",
+  redirect: "follow" as RequestRedirect | undefined,
+};
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
@@ -15,31 +21,30 @@ const Home: NextPage = () => {
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">NectarFinance!</span>
           </h1>
-          <div className="flex justify-center items-center space-x-2">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
-          </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
+          <p className="text-center">Connect your wallet to get started!</p>
+          {connectedAddress && (
+            <div className="flex justify-center items-center space-x-2">
+              <p className="my-2 font-medium">Connected Address:</p>
+              <Address address={connectedAddress} />
+              <Button
+                onClick={() => {
+                  const url = `${baseURL}/getNFTs/?owner=${connectedAddress}`;
+
+                  fetch(url, requestOptions)
+                    .then(response => response.json())
+                    .then(result => console.log(result))
+                    .catch(error => console.log("error", error));
+                }}
+              >
+                Fetch NFTs
+              </Button>
+            </div>
+          )}
         </div>
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
+        {/* <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
               <BugAntIcon className="h-8 w-8 fill-secondary" />
@@ -62,7 +67,7 @@ const Home: NextPage = () => {
               </p>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
