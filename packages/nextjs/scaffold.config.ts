0f019a01c47b55f139dd9f1bece4252e80c33945
William Wang <bwilliamwang@gmail.com>
2024-04-19 00:54:49 -0400

    morph target chain

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -36,7 +36,7 @@ export const morphTestnet = defineChain({
 
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.hardhat],
+  targetNetworks: [morphTestnet],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
